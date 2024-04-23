43856c2547b04b7d1806c01593cac4b713f51275
JohanBoDev <bohorquezjohan958@gmail.com>
2024-04-23 14:30:01 -0500

    actualizacion de js y css

--- script.js
+++ script.js
@@ -1,19 +1,4 @@
-<<<<<<< HEAD
-let boton = document.getElementById("icono");
-let enlaces = document.getElementById("enlaces");
-let contador = 0;
 
-boton.addEventListener("click", function(){
-    if(contador==0){
-        enlaces.className=("enlaces dos")
-        contador=1;
-    }else{
-        enlaces.classList.remove("dos")
-        enlaces.className= ("enlaces uno")
-        contador=0;
-    }
-})
-=======
 let boton = document.getElementById("icono");
 let enlaces = document.getElementById("enlaces");
 let galeria_port = document.getElementById("galeria-port");
@@ -32,6 +17,3 @@ boton.addEventListener("click", function(){
         galeria_port.style.display = "flex";
     }
 })
-
-
->>>>>>> f5b0675 (Actualizacion del menu, nuestras recetas y comidas)
