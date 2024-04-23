63f652c88a7fe7729d11c7eb81990206a1658149
JohanBoDev <bohorquezjohan958@gmail.com>
2024-04-23 14:23:56 -0500

    Actualizacion del menu, nuestras recetas y comidas

--- script.js
+++ script.js
@@ -1,3 +1,4 @@
+<<<<<<< HEAD
 let boton = document.getElementById("icono");
 let enlaces = document.getElementById("enlaces");
 let contador = 0;
@@ -12,3 +13,25 @@ boton.addEventListener("click", function(){
         contador=0;
     }
 })
+=======
+let boton = document.getElementById("icono");
+let enlaces = document.getElementById("enlaces");
+let galeria_port = document.getElementById("galeria-port");
+
+let contador = 0;
+
+boton.addEventListener("click", function(){
+    if(contador==0){
+        enlaces.className=("enlaces dos")
+        contador=1;
+        galeria_port.style.display = "none";
+    }else{
+        enlaces.classList.remove("dos")
+        enlaces.className= ("enlaces uno")
+        contador=0;
+        galeria_port.style.display = "flex";
+    }
+})
+
+
+>>>>>>> f5b0675 (Actualizacion del menu, nuestras recetas y comidas)
